aedae6714d23ab2eb66625bba5572018e90e57c5
lchabolla <lchabolla@gmail.com>
2018-11-03 21:34:16 -0700

    Added Study Drill questions to ex29

--- ex29.py
+++ ex29.py
@@ -18,10 +18,21 @@ if people > dogs:
 dogs += 5
 
 if people >= dogs:
-    print("People are great than or equal to dogs.")
+    print("People are greater than or equal to dogs.")
 
 if people <= dogs:
     print("People are less than or equal to dogs.")
 
 if people == dogs:
     print("People are dogs.")
+
+if people == dogs and people <= cats:
+    print("People are dogs and there are too many cats.")
+
+# Study drills
+# 1. The if statement run the code underneath the statement if it matches the condition
+# 2. The code needs to be indented so the python recognizes that this is the code that should be ran
+# if it matches the boolean condition
+# 3. If you don't indent you get an IndentationError
+# 4. Is on line 29 - 30
+# 5. If you change the initial values, the output may differ depending if the if conditions are met
